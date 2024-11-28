55484d5d8dda4447e3f0a0f522121100f569cebc
hxm <2962695969@qq.com>
2024-11-28 22:12:56 +0800

    2024-11-28 创建了globalmall_user模块和改变dubbo的group为DUBBO_GROUP

---
+++ globalmall-user/src/main/java/com/globalmall/GlobalmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.globalmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalmallUserApplication.class, args);
+    }
+
+}
